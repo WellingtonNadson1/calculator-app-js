1477950d71969d6143c2f3893799308a89bb383c
Wellington Nadson <76890847+WellingtonNadson1@users.noreply.github.com>
2021-12-31 16:05:32 -0300

    Change Layout

---
+++ Calculadora/js/logica.js
@@ -0,0 +1,6 @@
+let resultado = document.querySelector(".resultado");
+let btn9 = document.querySelector(".num-9");
+let btnZero = document.querySelector(".num-0");
+let btnSoma = document.querySelector(".soma");
+let btnDiv = document.querySelector(".dividir");
+
