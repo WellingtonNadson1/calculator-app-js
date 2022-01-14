4ac9ecde3a329e4b67ceb9fcef45b48502a9e21c
Wellington Nadson <76890847+WellingtonNadson1@users.noreply.github.com>
2022-01-14 13:01:24 -0300

    Criação de variáveis JS
    Mudanças nos botões do HTML e criação de variáveis a serem buscadas.

--- Calculadora/js/logica.js
+++ Calculadora/js/logica.js
@@ -1,6 +1,21 @@
-let resultado = document.querySelector(".resultado");
-let btn9 = document.querySelector(".num-9");
-let btnZero = document.querySelector(".num-0");
-let btnSoma = document.querySelector(".soma");
-let btnDiv = document.querySelector(".dividir");
+let btnZero = document.querySelector(".btn0");
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+let btnPonto = document.querySelector(".btnponto");
+let btnPorcento = document.querySelector(".btn%");
+let btnMultiplicar = document.querySelector("#multiplicar");
+let btnDividir = document.querySelector("#dividir");
+let btnSomar = document.querySelector("#somar");
+let btnSubtrair = document.querySelector("#subtrair");
+let btnReset = document.querySelector("#reset");
+let btnLimpar = document.querySelector("#limpar");
+let resultado = document.querySelector("#resultado");
+
 
