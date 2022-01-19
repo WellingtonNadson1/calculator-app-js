28208ab6d5b8c6d735f7f01b8d0d9ad57672df60
Wellington Nadson <76890847+WellingtonNadson1@users.noreply.github.com>
2022-01-18 21:13:02 -0300

    Calculadora já "Funcional"
    A lógica foi pré ajustada, ainda preciso melhorar.

--- Calculadora/js/logica.js
+++ Calculadora/js/logica.js
@@ -1,21 +1,97 @@
-let btnZero = document.querySelector(".btn0");
-let btn1 = document.querySelector(".btn1");
-let btn2 = document.querySelector(".btn2");
-let btn3 = document.querySelector(".btn3");
-let btn4 = document.querySelector(".btn4");
-let btn5 = document.querySelector(".btn5");
-let btn6 = document.querySelector(".btn6");
-let btn7 = document.querySelector(".btn7");
-let btn8 = document.querySelector(".btn8");
-let btn9 = document.querySelector(".btn9");
-let btnPonto = document.querySelector(".btnponto");
-let btnPorcento = document.querySelector(".btn%");
-let btnMultiplicar = document.querySelector("#multiplicar");
-let btnDividir = document.querySelector("#dividir");
-let btnSomar = document.querySelector("#somar");
-let btnSubtrair = document.querySelector("#subtrair");
-let btnReset = document.querySelector("#reset");
-let btnLimpar = document.querySelector("#limpar");
-let resultado = document.querySelector("#resultado");
+//Criando variáveis para armazenar valores
+const numeros = document.querySelectorAll('[class*=num]');
+const operadores = document.querySelectorAll(".operador");
+const btnPonto = document.querySelector(".btnponto");
+const btnPorcento = document.querySelector(".btnporcento");
+const btnReset = document.querySelector("#reset");
+console.log(btnReset);
+const btnLimpar = document.querySelector("#limpar");
+const btnIgual = document.querySelector("#igual");
+let display = document.querySelector("#display");
+
+let valorAtual = '';
+let valorAnterior = '';
+let operacao = undefined;
+
+/* Utilizando um forEach para percorrer todos os buttons salvos 
+na varíavel 'numeros' para Criar os eventos de cliques que irão 
+buscar e reconhecer cada Número que será clicado no HTML*/
+numeros.forEach(function (button){
+    button.addEventListener('click', function (){
+        juntarNumero(button.innerText);
+    })
+})
+
+/* Utilizando um forEach para percorrer todos os buttons salvos 
+na varíavel 'operadores' para Criar os eventos de cliques que irão 
+buscar e reconhecer cada operador clicado no HTML*/
+operadores.forEach(function (button){
+    button.addEventListener('click', function (){
+        selecionarOperacao(button.innerText);
+    })
+})
+
+/*Criando evento de cique para o button 'Igual'*/
+btnIgual.addEventListener('click', function(){
+    calcular();
+    atualizarDisplay();
+})
 
 
+btnReset.addEventListener('click', function(){
+    clear();
+    atualizarDisplay();
+})
+
+function juntarNumero(num){
+    valorAtual = valorAtual.toString() + num.toString();
+    atualizarDisplay();
+}
+
+function atualizarDisplay(){
+    display.value = valorAtual;
+}
+
+function clear(){
+    valorAtual = "";
+    valorAnterior = "";
+    operacao = undefined;
+}
+
+function selecionarOperacao(operador){
+    if (valorAtual === "") return;
+    if (valorAnterior !== ""){
+        calcular();
+    }
+    operacao = operador.toString();
+    valorAnterior = valorAtual;
+    valorAtual = "";
+}
+
+function calcular(){
+    let calculo;
+    const anterior = parseFloat(valorAnterior);
+    const atual = parseFloat(valorAtual);
+    if (isNaN(anterior) || isNaN(atual)) return;
+    switch(operacao){
+        case '+':
+            calculo = anterior + atual;
+            break;
+        case '-':
+            calculo = anterior - atual;
+            break;
+        case '/':
+            calculo = anterior / atual;
+            break;
+        case 'x':
+            calculo = anterior * atual;
+            break;
+        default:
+            return;
+    }
+    valorAtual = calculo;
+    valorAnterior = "";
+    operacao = undefined;
+}
+
+clear();
